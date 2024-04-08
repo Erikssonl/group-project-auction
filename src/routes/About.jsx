13b929420e9334701c51314f92845c7b1582d412
Emelie Forselius <emelieforselius@outlook.com>
2024-04-08 10:24:57 +0200

    about comp styled

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,52 +1,67 @@
+import "../styles/aboutcomp-style.css";
+
 function About() {
   return (
-    <>
+    <div className="about-wrapper">
       <div>
-        <h1>Om Auctioneer</h1>
+        <h1> <br />Om Auctioneer</h1>
+        <br />
       </div>
-      <div>
+      <div className="about-info">
         <p>
           Välkommen till vår auktionsplattform, en digital mötesplats för
           älskare av unika föremål och kvalitetsfynd. Vi är en bro mellan
           prestigefyllda europeiska auktionshus och entusiaster som söker det
           exceptionella inom konst, design, antika möbler, samlarobjekt och
           mycket annat.
-        </p>
-
-        <p>
+          <br /> <br />
           Vår filosofi grundar sig på en tro att varje föremål har en historia
           att berätta. Därför ser vi till att allt som listas på vår webbplats
           har genomgått en noggrann granskning av våra experter. Varje artikel
           är detaljerat beskriven och professionellt fotograferad för att du som
           köpare ska känna dig helt trygg i ditt beslut. Vi lovar inte bara
           kvalitet; vi garanterar den.
-        </p>
-
-        <p>
+          <br /> <br />
           Vi är övertygade om att magin i att upptäcka och förvärva unika
           föremål inte bara berikar våra hem, utan även våra liv. Oavsett om du
           är en inbiten samlare eller på jakt efter det där speciella tillägget
           till ditt hem, erbjuder vi en plattform som förenar tradition med
           modern bekvämlighet. Upptäck, buda och förvandla ditt hem med oss.
           Välkommen till en värld av unika fynd och tidlösa skatter.
+          <br /> <br />
         </p>
       </div>
-      <div>
+      
+      <div className="contact-info">
         <h1>Kontakta oss!</h1>
-      </div>
-      <div>
-        <p>Har du frågor? Tveka inte på att kontakta oss!</p>
+        <br />
+        <p>
+          Har du frågor? Tveka inte på att kontakta oss! <br />
+          <br />
+        </p>
         <p>
           Våra telefontider är: <br />
           Mån-fre 9-18 <br />
           Lör-Sön STÄNGT
+          <br />
+          <br />
         </p>
-        <p>Telefonnummer: 073-637 73 82</p>
-        <p>Du kan även kontakta oss på mail: <a href="mailto:auctioneer@gmail.com">auctioneer@gmail.com</a> </p>
-        <h2>Här finns vi</h2>
+        <p>
+          Telefonnummer: 073-637 73 82 <br />
+          <br />
+        </p>
+        <p>
+          Du kan även kontakta oss på mail:{" "}
+          <a href="mailto:auctioneer@gmail.com">auctioneer@gmail.com</a> <br />
+          <br />{" "}
+        </p>
+        <h2>
+          Här finns vi <br />
+          <br />
+        </h2>
         <p>Kistavägen 47, 164 74 Kista</p>
       </div>
-    </>
+    </div>
   );
 }
 export default About;
