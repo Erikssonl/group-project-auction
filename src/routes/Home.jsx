82356a80e90592860d783ef01f5aa09934ece8ae
Louise Eriksson <louiseriksson.92@gmail.com>
2024-04-08 09:33:48 +0200

    small change

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
     useEffect (() => {
         const getFromAuctionAPI = () => {
 
-            fetch("https://auctioneer.azurewebsites.net/auction/3tsr")
+            fetch("https://auctioneer2.azurewebsites.net/auction/3tsr")
                 .then(response => response.json())
                 .then(data => {
                     const currentDate = new Date();
