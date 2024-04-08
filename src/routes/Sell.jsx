b27dcdd9977e722dc233c52685ca2492ae95f16b
Louise Eriksson <louiseriksson.92@gmail.com>
2024-04-08 10:27:50 +0200

    starting on form

--- src/routes/Sell.jsx
+++ src/routes/Sell.jsx
@@ -5,6 +5,13 @@ const Sell = () => {
   return (
     <div className='sell-wrap'>
       <Sellinfocomp/>
+
+      <div className='sellForm'>
+        <h2 className='sell-title'>Vad vill du sälja?</h2>
+        <p>Title</p>
+        <input type="text" placeholder='Ange föremålets titel...' />
+        <p>Beskrivning</p>
+      </div>
     </div>
   )
 }
